9a5d25358284cfc44d6f95b88e77605cd176db4b
Shane K Johnson <shjohnso@redhat.com>
2012-05-01 22:11:18 -0500

    formatting

--- trading-services/src/main/java/com/jboss/trading/services/util/Resources.java
+++ trading-services/src/main/java/com/jboss/trading/services/util/Resources.java
@@ -1,10 +1,9 @@
 package com.jboss.trading.services.util;
 
-import javax.ejb.EJB;
-import javax.enterprise.inject.Produces;
-
 import com.jboss.trading.api.TradeManager;
 import com.jboss.trading.services.TradeManagerLocal;
+import javax.ejb.EJB;
+import javax.enterprise.inject.Produces;
 import javax.inject.Named;
 
 public class Resources {
